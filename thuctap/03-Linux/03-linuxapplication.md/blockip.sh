707b6cdec336da939826bb25e20317722ac7794a
thanhquang99 <khuongthanhquang276@gmail.com>
2024-02-23 11:40:51 +0700

    Update blockip.sh

--- thuctap/03-Linux/03-linuxapplication.md/blockip.sh
+++ thuctap/03-Linux/03-linuxapplication.md/blockip.sh
@@ -2,45 +2,33 @@
 
 LOG_FILE="/var/log/secure"
 STATE_FILE="/tmp/bad_ip"
-BAN_TIME=600 # 10 phút
 
 # Tạo tệp bad_ip nếu nó không tồn tại
 touch "$STATE_FILE"
 
-while true; do
-    current_time=$(date "+%b %d %H:%M:%S")
-    one_minute_ago=$(date -d '1 minute ago' '+%b %d %H:%M:%S')
+# Lấy ip ssh failed trong 1 phút
+current_time=$(date "+%b %d %H:%M:%S")
+one_minute_ago=$(date -d '1 minute ago' '+%b %d %H:%M:%S')
+formatted_time1=$(date -d "$one_minute_ago" "+%d%H%M%S")
+formatted_time=$(date -d "$current_time" "+%d%H%M%S")
 
-    failed_ips=($(grep "sshd.*Failed password" "$LOG_FILE" | awk -v start_time="$one_minute_ago" -v end_time="$current_time" '$1 " " $2 " " $3 >= start_time && $1 " " $2 " " $3 <= end_time {print $(NF-3)}' | sort | uniq -c | awk '$1 > 3 {print $2}'))
+failed_ips=($(grep "sshd.*Failed password" "$LOG_FILE" | awk -v start_time="$one_minute_ago" -v end_time="$current_time" '$1 " " $2 " " $3 >= start_time && $1 " " $2 " " $3 <= end_time {print $(NF-3)}' | sort | uniq -c | awk '$1 > 3 {print $2}'))
 
-    # Lấy thời gian từ file /tmp/bad_ip
-    time_ip=$(awk '{print $1, $2, $3}' "$STATE_FILE")
-    current_timestamp=$(date +%s)
-    time_difference=$((current_timestamp - timestamp_ip))
+if [ ${#failed_ips[@]} -gt 0 ]; then
+    for failed_ip in "${failed_ips[@]}"; do
+        sudo iptables -A INPUT -s "$failed_ip" -j DROP
+        echo "$formatted_time $failed_ip" >> "$STATE_FILE"
+    done
+fi
 
-    # Chặn địa chỉ IP nếu có
-    if [ ${#failed_ips[@]} -gt 0 ]; then
-        for failed_ip in "${failed_ips[@]}"; do
-            iptables -A INPUT -s "$failed_ip" -j DROP
-            echo "$(date "+%b %d %H:%M:%S") $failed_ip" >> "$STATE_FILE"
-        done
-        service iptables save
-        service iptables reload
-    fi
+# Unban IP sau thời gian đủ
+ip_unbans=($(grep "$formatted_time1" "$STATE_FILE" | awk '{print $2}'))
 
-    # Kiểm tra và unban IP nếu đã qua thời gian quy định
-    while read -r line; do
-        entry_time=$(echo "$line" | awk '{print $1, $2, $3}')
-        if [ "$entry_time" \< "$current_time" ]; then
-            unban_ip=$(echo "$line" | awk '{print $4}')
-            iptables -D INPUT -s "$unban_ip" -j DROP
-            # Xóa chỉ những IP đã qua thời gian quy định
-            grep -v "$line" "$STATE_FILE" > /tmp/bad_ip_tmp
-            mv /tmp/bad_ip_tmp "$STATE_FILE"
-            service iptables save
-            service iptables reload
-        fi
-    done < "$STATE_FILE"
-
-    sleep 60
-done
+if [ ${#ip_unbans[@]} -gt 0 ]; then
+    for ip_unban in "${ip_unbans[@]}"; do
+        echo $ip_unban
+        sudo iptables -D INPUT -s "$ip_unban" -j DROP
+    done
+fi
+sudo service iptables save
+sudo service iptables reload
